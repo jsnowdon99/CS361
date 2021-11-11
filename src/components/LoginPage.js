ffb0bc6199f1c1ebafaf5429c851e6ed7d8a3ed3
Jack Snowdon <jacksnowdon99@gmail.com>
2021-11-10 22:53:26 -0800

    continuing to add functionality to login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,65 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import BeerPage from "../components/BeerPage";
+import axios from 'axios';
 
 const LoginPage = () => {
     const [email, setEmail] = useState('')
+    const [userID, setID] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState(null)
     const navigate = useNavigate();
     const loginSuccess = () => navigate('/beers')
+    const flowIDs = []
+    const addEmail = () => {
+        // alert("Please check your email");
+
+        axios.post('https://concise-bloom-327806.wl.r.appspot.com/2fa', {
+            email: email,
+            }).then(response => {
+                console.log('response >>> ', response);
+                let id = response.data.account_id
+                alert("Your user ID is \n" + id + "\nPlease save this for future login")
+            }).catch(error => {
+                alert("This email is already associated with an account")
+            });
+    }
+
+    const attemptLogin = () => {
+        axios.post('https://concise-bloom-327806.wl.r.appspot.com/2fa/flow', {
+            account_id: parseInt(userID),
+            }).then(response => {
+                console.log('response >>> ', response);
+                let flow = response.data.flow_id
+                flowIDs.push(flow)
+                console.log(flowIDs)
+                alert("Check your email for a code")
+                
+            }).catch(error => {
+                console.log(error)
+                alert("This is not a valid ID")
+            });
+    }
     
+    const confirmPassword = () => {
+        axios.put('https://concise-bloom-327806.wl.r.appspot.com/2fa/flow/', {
+            code: parseInt(password),
+            }).then(response => {
+                console.log('response >>> ', response);
+                
+                if (response.data.authenticated == true) {
+                    loginSuccess()
+                }
+            }).catch(error => {
+                alert("This is not a valid code")
+            });
+    }
+    
+
     return (
         <div>
-            Please enter your email:
+            If you are a first time user, please enter your email:
                 <div>
-                <br/>
                     Email: <br/>
                     <input type="text"
                     value={email}
@@ -21,8 +67,19 @@ const LoginPage = () => {
                 
                 </div>
                 <div><input type="button"
-                    value="Retrieve one time password"/></div>
+                    value="Join"
+                    onClick={addEmail}/></div>
                 <br/>
+                <div>
+                    Existing members, please use your ID: <br/>
+                    <input type="number"
+                    value={userID}
+                    onChange={e => setID(e.target.value)}/>
+                
+                </div>
+                <input type="button"
+                    value="Get my one time password"
+                    onClick={attemptLogin}/>
                 <div>
                     Enter your one time password:<br/>
                     <input type="password"
@@ -34,7 +91,7 @@ const LoginPage = () => {
                 <div>
                     <input type="button"
                     value="Login"
-                    onClick={loginSuccess}/>
+                    onClick={confirmPassword}/>
                 </div>
             </div>
         
